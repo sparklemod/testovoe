85dd46ae6f1e776fd99236f46b48b1fff1be4e38
sparklemod <katerina5314@mail.ru>
2022-10-28 23:01:01 +0300

    first commit

---
+++ 2_index.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+
+<form action="2_upload.php" method="post" enctype="multipart/form-data">
+    Загрузить этот файл: <input type="file" name="import_file"/>
+    <input type="submit" value="Отправить" />
+</form>
+
+</body>
---
+++ 2_upload.php
@@ -0,0 +1,33 @@
+<?php
+$new_name = trim(mb_strtolower($_FILES['import_file']['name']));
+$csvFileType = strtolower(pathinfo($new_name, PATHINFO_EXTENSION));
+
+if('POST' == $_SERVER['REQUEST_METHOD'] && isset($_FILES['import_file']) && ($csvFileType == "csv") && ($_FILES["import_file"]["size"] < 3000)) {
+    $handle = fopen($_FILES['import_file']['tmp_name'], "r");
+    if (false === $handle){
+        die('Can\'t open file');
+    }
+    else{
+        $dir = "upload";
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777);
+        }
+        $array_csv = [];
+        while (false !== ($row = fgetcsv($handle,1000,','))){
+            $array_csv[] = $row;
+        }
+        fclose($handle);
+
+        //echo "<pre>".print_r($array_csv, 1) . "</pre>";
+        for ($i = 0; $i < count($array_csv); $i++) {
+            $extention = substr($array_csv[$i][0], strpos($array_csv[$i][0],"."));
+            //$filename = $uploadDir . ($i+1) . $extention;
+            $filename = __DIR__ . '/upload/' . ($i+1) . $extention;
+            echo "<pre>" . ($i+1) . $extention . ' was created' . "</pre>";
+            file_put_contents($filename, $array_csv[$i][1]);
+        }
+
+    }
+}
+else echo "Возможно, файл не csv формата или слишком большой. Попробуйте снова";
+?>
